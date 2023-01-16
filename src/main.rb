385858389b059c054e5d50097b3de001183dcfda
ntzex <79799006+ntzex@users.noreply.github.com>
2023-01-16 17:28:09 +0300

    Edited with linter

--- src/main.rb
+++ src/main.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: false
+
 require 'yaml'
 require 'telegram/bot'
 
-API_KEY = ''
+API_KEY = ""
 TARGET_CHAT_ID = # т.к. я его заточил под один чат
 
-yaml_file = File.open("db.yaml")
-Messages = YAML.load(yaml_file.read)
+yaml_file = File.open('db.yaml')
+Messages = YAML.safe_load(yaml_file.read)
 puts "Loaded #{Messages.size} messages"
 
 Telegram::Bot::Client.run(API_KEY) do |bot|
@@ -13,8 +15,8 @@ Telegram::Bot::Client.run(API_KEY) do |bot|
     if message.methods.include?(:text) && message.chat.id == TARGET_CHAT_ID
       case message.text
       when nil
-        puts "I caught nil, ignoring"
-      when "Сколько сообщений"
+        puts 'I caught nil, ignoring'
+      when 'Сколько сообщений'
         bot.api.send_message(chat_id: message.chat.id, text: "Я знаю #{Messages.size} сообщений")
       else
         puts "(#{Messages.size}) Appended #{message.text}"
@@ -23,8 +25,8 @@ Telegram::Bot::Client.run(API_KEY) do |bot|
 
       if rand(0..6) == 1
         bot.api.send_message(chat_id: message.chat.id, text: Messages.sample)
-        File.write("db.yaml", Messages.to_yaml) # вынесено т.к. с большой базой сообщений так можно ускорить принятие сообщений
-        puts "Sent new random message and saved db"
+        File.write('db.yaml', Messages.to_yaml) # вынесено т.к. с большой базой сообщений так можно ускорить принятие сообщений
+        puts 'Sent new random message and saved db'
       end
     end
   end
